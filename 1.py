d157f8ecedb8906ce40193bf8ccc37ff47eed157
Zarathustra123 <157049910+zarathustra2024@users.noreply.github.com>
2024-03-16 13:40:48 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,21 @@
+def total_salary(path: str):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            salaries = [int(line.strip().split(',')[1]) for line in file]
+
+        total_sum = sum(salaries)
+        average_salary = total_sum / len(salaries)
+        return total_sum, average_salary
+
+    except FileNotFoundError:
+        print("Файл не знайдено. Перевірте правильність шляху до файлу.")
+        return None, None
+    except ValueError:
+        print("Помилка при обробці даних. Переконайтеся, що дані в файлі відформатовані коректно.")
+        return None, None
+
+
+total_sum, average_salary = total_salary("E:/Python/Homework_4/Alex Korp.txt")
+if total_sum is not None and average_salary is not None:
+    print(
+        f"Загальна сума заробітної плати: {total_sum}, Середня заробітна плата: {average_salary}")
